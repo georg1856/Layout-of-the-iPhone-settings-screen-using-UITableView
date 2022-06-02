57670d51a2f694cb03220e4b62bfe7f9122b5eda
Georgiy <33875528+georg1856@users.noreply.github.com>
2022-06-02 16:52:11 +1000

    creating cells with the type: static

--- Layout-of-the-iPhone-settings-screen-using-UITableView/Layout-of-the-iPhone-settings-screen-using-UITableView/AppDelegate.swift
+++ Layout-of-the-iPhone-settings-screen-using-UITableView/Layout-of-the-iPhone-settings-screen-using-UITableView/AppDelegate.swift
@@ -13,7 +13,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if #available(iOS 15, *) {
+              let navigationBarAppearance = UINavigationBarAppearance()
+              navigationBarAppearance.configureWithDefaultBackground()
+              UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+              UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+
+              let tabBarAppearance = UITabBarAppearance()
+              tabBarAppearance.configureWithDefaultBackground()
+              UITabBar.appearance().standardAppearance = tabBarAppearance
+              UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+            }
         return true
     }
 
