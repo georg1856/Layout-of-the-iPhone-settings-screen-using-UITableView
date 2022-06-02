01d35e130de08046d37b62da049448873898d3eb
Georgiy <33875528+georg1856@users.noreply.github.com>
2022-06-02 16:40:58 +1000

    Creating simple table, declaration of required methods
    as well as creating an extension for UIViewController

--- Layout-of-the-iPhone-settings-screen-using-UITableView/Layout-of-the-iPhone-settings-screen-using-UITableView/Extension.swift
+++ Layout-of-the-iPhone-settings-screen-using-UITableView/Layout-of-the-iPhone-settings-screen-using-UITableView/Extension.swift
@@ -6,3 +6,99 @@
 //
 
 import Foundation
+import UIKit
+
+struct Section {
+    let option: [SettingsOptionType]
+}
+
+enum SettingsOptionType {
+    case staticCell(model: SettingsOption)
+    case switchCell(model: SettingsSwitchOption)
+    case someInfoCell(model: SettingInfo)
+    case notificationCell(model: SettingNotification)
+}
+
+struct SettingsOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroungColor: UIColor
+    let handler: ((_ doubleHeader: String) -> Void)
+}
+
+struct SettingsSwitchOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroungColor: UIColor
+    var isOn: Bool
+    let handler: ((_ doubleHeader: String) -> Void)
+}
+
+struct SettingInfo {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroungColor: UIColor
+    var titleInfo: String
+    let handler: ((_ doubleHeader: String) -> Void)
+}
+
+struct SettingNotification {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroungColor: UIColor
+    var titleInfo: String
+    let handler: ((_ doubleHeader: String) -> Void)
+}
+
+extension ViewController {
+
+    enum CellTitle: String {
+        case sectionOneCellOne = "Авиарежим"
+        case sectionOneCellTwo = "Wi-Fi"
+        case sectionOneCellThree = "Bluetooth"
+        case sectionOneCellFour = "Сотовая связь"
+        case sectionOneCellFive = "Режим модема"
+        case sectionOneCellSix = "VPN"
+        
+        case sectionTwoCellOne = "Уведомления"
+        case sectionTwoCellTwo = "Звуки, тактильные сигналы"
+        case sectionTwoCellThree = "Не беспокоить"
+        case sectionTwoCellFour = "Экранное время"
+        
+        case sectionThreeCellOne = "Основные"
+        case sectionThreeCellTwo = "Пункт управления"
+        case sectionThreeCellThree = "Экран и яркость"
+        case sectionThreeCellFour = "Экран «Домой»"
+        case sectionThreeCellFive = "Универсальный доступ"
+    }
+    
+    enum Image: String {
+        case sectionOneCellOne = "airplane"
+        case sectionOneCellTwo = "wifi"
+        case sectionOneCellThree = "bluetooth"
+        case sectionOneCellFour = "antenna.radiowaves.left.and.right"
+        case sectionOneCellFive = "personalhotspot"
+        case sectionOneCellSix = "vpn"
+
+        case sectionTwoCellOne = "app.badge"
+        case sectionTwoCellTwo = "speaker.wave.3.fill"
+        case sectionTwoCellThree = "moon.fill"
+        case sectionTwoCellFour = "hourglass"
+
+        case sectionThreeCellOne = "gear"
+        case sectionThreeCellTwo = "switch.2"
+        case sectionThreeCellThree = "character"
+        case sectionThreeCellFour = "house.circle"
+        case sectionThreeCellFive = "figure.walk.circle"
+    }
+    
+    enum Color {
+        static var firstColor = UIColor.systemOrange
+        static var secondColor = UIColor.systemBlue
+        static var thirdColor = UIColor.systemGreen
+        static var fourColor = UIColor.link
+        static var fiveColor = UIColor.systemPink
+        static var sixColor = UIColor.systemGray
+        static var sevenColor = UIColor.blue
+    }
+}
--- Layout-of-the-iPhone-settings-screen-using-UITableView/Layout-of-the-iPhone-settings-screen-using-UITableView/ViewController.swift
+++ Layout-of-the-iPhone-settings-screen-using-UITableView/Layout-of-the-iPhone-settings-screen-using-UITableView/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    var models = [Section]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configure()
+    }
+    
+    func configure() {
+    
     }
 
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models[section].option.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let type = models[indexPath.section].option[indexPath.row]
+    }
 }
 
